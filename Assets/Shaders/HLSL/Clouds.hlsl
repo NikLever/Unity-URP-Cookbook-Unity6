305844189ee539993e1954f802a574103b5cace7
Nik Lever <47000827+NikLever@users.noreply.github.com>
2024-10-30 08:03:09 +0000

    Working on Clouds in Procedural Noise

--- Assets/Shaders/HLSL/Clouds.hlsl
+++ Assets/Shaders/HLSL/Clouds.hlsl
@@ -1,48 +1,23 @@
-#include "./noiseSimplex.hlsl"
-
-//Uses 3D texture and lighting 
-void clouds_float( float3 rayOrigin, float3 rayDirection, float numSteps, float stepSize,
-    float densityScale, float noiseScale,
-    float3 offset, float numLightSteps, float lightStepSize, float3 lightDir,
-    float lightAbsorb, float darknessThreshold, float transmittance, out float3 result )
-{
-float density = 0;
-float transmission = 0;
-float lightAccumulation = 0;
-float finalLight = 0;
-
-
-for(int i =0; i< numSteps; i++){
-    rayOrigin += (rayDirection*stepSize);
-
-    //The blue dot position
-    float3 samplePos = rayOrigin+offset;
-    float sampledDensity = snoise(samplePos * noiseScale);
-    density += sampledDensity*densityScale;
-
-    //light loop
-    float3 lightRayOrigin = samplePos;
-
-    for(int j = 0; j < numLightSteps; j++){
-        //The red dot position
-        lightRayOrigin += -lightDir*lightStepSize;
-        float lightDensity = snoise(lightRayOrigin * noiseScale);
-        //The accumulated density from samplePos to the light - the higher this value the less light reaches samplePos
-        lightAccumulation += lightDensity;
-    }
-
-    //The amount of light received along the ray from param rayOrigin in the direction rayDirection
-    float lightTransmission = exp(-lightAccumulation);
-    //shadow tends to the darkness threshold as lightAccumulation rises
-    float shadow = darknessThreshold + lightTransmission * (1.0 -darknessThreshold);
-    //The final light value is accumulated based on the current density, transmittance value and the calculated shadow value 
-    finalLight += density*transmittance*shadow;
-    //Initially a param its value is updated at each step by lightAbsorb, this sets the light lost by scattering
-    transmittance *= exp(-density*lightAbsorb);
+// Returns (dstToBox, dstInsideBox). If ray misses box, dstInsideBox will be zero
+float2 rayBoxDst(float3 boundsMin, float3 boundsMax, float3 rayOrigin, float3 invRaydir) {
+    // Adapted from: http://jcgt.org/published/0007/03/04/
+    float3 t0 = (boundsMin - rayOrigin) * invRaydir;
+    float3 t1 = (boundsMax - rayOrigin) * invRaydir;
+    float3 tmin = min(t0, t1);
+    float3 tmax = max(t0, t1);
     
-    }
+    float dstA = max(max(tmin.x, tmin.y), tmin.z);
+    float dstB = min(tmax.x, min(tmax.y, tmax.z));
 
-    transmission = exp(-density);
+    // CASE 1: ray intersects box from outside (0 <= dstA <= dstB)
+    // dstA is dst to nearest intersection, dstB dst to far intersection
 
-    result = float3(finalLight, transmission, transmittance);
+    // CASE 2: ray intersects box from inside (dstA < 0 < dstB)
+    // dstA is the dst to intersection behind the ray, dstB is dst to forward intersection
+
+    // CASE 3: ray misses box (dstA > dstB)
+
+    float dstToBox = max(0, dstA);
+    float dstInsideBox = max(0, dstB - dstToBox);
+    return float2(dstToBox, dstInsideBox);
 }
